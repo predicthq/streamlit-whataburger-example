34cce2323c8f94af5fe3803d58f64e567e969d38
Glen Alexander <49417961+glenax@users.noreply.github.com>
2023-04-25 07:44:51 -0700

    Update demand_surge.py

--- pages/demand_surge.py
+++ pages/demand_surge.py
@@ -44,7 +44,7 @@ def demand_surge():
         return
     
     st.markdown(
-        "<img src='app/static/1200px-Dickeys_Barbecue_Pit_Logo.png' width='160' />", unsafe_allow_html=True
+        "<img src='app/static/Whataburger_logo.png' width='160' />", unsafe_allow_html=True
     )
     st.header(location["name"])
 
